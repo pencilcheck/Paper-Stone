b3f50e3de167e8621f7b8ecbe9248e2e3739aa5d
Penn Su <pennsuaccount@gmail.com>
2012-12-29 03:35:06 +0800

    Initial Commit

---
+++ Paper Stone/PSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PSAppDelegate.h
+//  Paper Stone
+//
+//  Created by Penn Su on 12/29/12.
+//  Copyright (c) 2012 Light Years. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Paper Stone/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Paper Stone
+//
+//  Created by Penn Su on 12/29/12.
+//  Copyright (c) 2012 Light Years. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PSAppDelegate class]));
+    }
+}
